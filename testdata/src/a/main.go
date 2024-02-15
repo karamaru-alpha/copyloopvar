e9ea31662e0649a3d24d18ebda8e9f70651e2cfa
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-02-15 20:08:37 +0100

    feat: improve report message

--- testdata/src/a/main.go
+++ testdata/src/a/main.go
@@ -2,20 +2,20 @@ package a
 
 func f() {
 	for i, v := range []int{1, 2, 3} {
-		i := i  // want `It's unnecessary to copy the loop variable "i"`
-		_v := v // want `It's unnecessary to copy the loop variable "v"`
+		i := i  // want `The copy of the 'for' variable "i" can be deleted \(Go 1\.22\+\)`
+		_v := v // want `The copy of the 'for' variable "v" can be deleted \(Go 1\.22\+\)`
 		_ = i
 		_ = _v
 	}
 
 	for i := 1; i <= 3; i++ {
-		i := i // want `It's unnecessary to copy the loop variable "i"`
+		i := i // want `The copy of the 'for' variable "i" can be deleted \(Go 1\.22\+\)`
 		_ = i
 	}
 
 	for i, j := 1, 1; i+j <= 3; i++ {
-		i := i       // want `It's unnecessary to copy the loop variable "i"`
-		j, _ := j, 1 // want `It's unnecessary to copy the loop variable "j"`
+		i := i       // want `The copy of the 'for' variable "i" can be deleted \(Go 1\.22\+\)`
+		j, _ := j, 1 // want `The copy of the 'for' variable "j" can be deleted \(Go 1\.22\+\)`
 		_, _ = i, j
 	}
 }
