987eaeddefbd949dde9cb20c29817b85335d9100
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-12-27 23:41:42 +0100

    tests: remove useless go.mod

--- copyloopvar_test.go
+++ copyloopvar_test.go
@@ -3,14 +3,12 @@ package copyloopvar
 import (
 	"testing"
 
-	"github.com/gostaticanalysis/testutil"
 	"golang.org/x/tools/go/analysis/analysistest"
 )
 
 func TestAnalyzer(t *testing.T) {
 	t.Run("basic", func(t *testing.T) {
-		testdata := testutil.WithModules(t, analysistest.TestData(), nil)
-		analysistest.Run(t, testdata, NewAnalyzer(), "basic")
+		analysistest.Run(t, analysistest.TestData(), NewAnalyzer(), "basic")
 	})
 
 	t.Run("check-alias", func(t *testing.T) {
@@ -18,7 +16,7 @@ func TestAnalyzer(t *testing.T) {
 		if err := analyzer.Flags.Set("check-alias", "true"); err != nil {
 			t.Error(err)
 		}
-		testdata := testutil.WithModules(t, analysistest.TestData(), nil)
-		analysistest.Run(t, testdata, analyzer, "checkalias")
+
+		analysistest.Run(t, analysistest.TestData(), analyzer, "checkalias")
 	})
 }
